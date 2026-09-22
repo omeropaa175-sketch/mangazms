4ad775342e101d5ba1560dd9d312a162cb0ab969
zaid <omeropaa175@gmail.com>
2026-09-22 18:29:26 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const url = process.env.SUPABASE_URL || '';
-  const anonKey = process.env.SUPABASE_ANON_KEY || '';
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).json({ url, anonKey });
-}
